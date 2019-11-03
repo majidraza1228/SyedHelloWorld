346c9438a7e6d9a0f6db95e181fb65c6b83f0b85
Syed Raza <majidraza@hotmail.com>
2019-11-03 08:34:25 -0500

    Sunday check in

---
+++ src/main/java/com/coveros/demo/helloworld/PaiStar.java
@@ -0,0 +1,28 @@
+package com.coveros.demo.helloworld;
+
+public class PaiStar {
+
+    public String PaiStar(String str) {
+        String tempPai = "";
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (((str.charAt(i) == str.charAt(i + 1)) )
+            ) {
+                tempPai = tempPai+str.charAt(i) + "*" + str.charAt(i + 1);}
+                else  {
+                    if (str.charAt(i) == str.charAt(i + 1) && tempPai.length()>2)
+                      tempPai =  tempPai+"*" + str.charAt(i) + "*" + str.charAt(i + 1);
+                }
+            }
+                return tempPai;
+    }
+
+
+
+    public static    void main(String[] str){
+
+        PaiStar abc = new PaiStar();
+
+        System.out.println(abc.PaiStar("bbb"));
+    }
+}
