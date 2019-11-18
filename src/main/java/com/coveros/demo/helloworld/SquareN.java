2336d7c229b97386b549ce3b573edb3d03070c0a
Syed Raza <majidraza@hotmail.com>
2019-11-18 00:44:19 -0500

    8

---
+++ src/main/java/com/coveros/demo/helloworld/SquareN.java
@@ -0,0 +1,28 @@
+package com.coveros.demo.helloworld;
+
+public class SquareN {
+
+    public static int squareSum(int[] n) {
+        //Your Code
+        int sumsq = 0;
+
+        for ( int number:n)
+            sumsq += number* number;
+/*
+        for (int i = 0; i < n.length; i++) {
+
+            sumsq = sumsq + (n[i] * n[i]);
+        }
+*/
+        return sumsq;
+    }
+
+    public static void main(String[] args){
+
+        int[] a={5,-3,4};
+
+        System.out.print(squareSum(a));
+
+    }
+
+}
